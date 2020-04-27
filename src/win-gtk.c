0924d6a168edc9c3555bec3bfd0789f040a9bf1f
hatsugai <hatsugai@principia-m.com>
2020-04-28 00:08:56 +0900

    G_APPLICATION_NON_UNIQUE

--- src/win-gtk.c
+++ src/win-gtk.c
@@ -727,7 +727,7 @@ int main(int argc, char **argv)
     argv_dummy[0] = argv[0];
     res_book_init();
     caml_main(argv);
-    app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    app = gtk_application_new("org.gtk.example", G_APPLICATION_NON_UNIQUE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), argv_dummy);
     int status = g_application_run(G_APPLICATION(app), argc_dummy, argv_dummy);
     g_object_unref(app);
